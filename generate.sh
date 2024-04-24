5631109981cd114d39a7f3c9793c486c9a20ab14
Laust Rud Jacobsen <lrj@biq.dk>
2024-04-24 11:09:29 +0200

    Update code-generation provider
    Switching to the maintained fork
    https://github.com/OpenAPITools/openapi-generator/blob/master/docs/migration-from-swagger-codegen.md

--- generate.sh
+++ generate.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 set -xe
 
-docker run --rm -v ${PWD}:/local swaggerapi/swagger-codegen-cli:2.4.0-elixir-uuid generate \
+docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate \
   -t /local/template \
   -i https://api.ynab.com/papi/open_api_spec.yaml \
   --invoker-package YNAB \
-  -l elixir \
+  -g elixir \
   -o /local
 
 mix format "lib/ynab/api/*.{ex}" "lib/ynab/model/*.{ex}"
