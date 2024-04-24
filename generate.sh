56eeb0244c46e7156f3d612517e237297d43d097
Laust Rud Jacobsen <lrj@biq.dk>
2024-04-24 10:33:57 +0200

    Update YNAB OpenAPI URL

--- generate.sh
+++ generate.sh
@@ -3,7 +3,7 @@ set -xe
 
 docker run --rm -v ${PWD}:/local swaggerapi/swagger-codegen-cli:2.4.0-elixir-uuid generate \
   -t /local/template \
-  -i https://api.youneedabudget.com/papi/spec-v1-swagger.json \
+  -i https://api.ynab.com/papi/open_api_spec.yaml \
   --invoker-package YNAB \
   -l elixir \
   -o /local
